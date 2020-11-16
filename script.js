f9e705cf5e781a03e33675ba80ac8064245c625f
Eric Hunter <ehunter7@live.com>
2020-11-15 20:47:24 -0800

    adding beginning notes to project

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword();  //Calls funtion to randomly genereat password based on parameters
+  var passwordText = document.querySelector("#password"); //selects the password id in html
 
-  passwordText.value = password;
+  passwordText.value = password; //Displays password to page
 
+}
+function generatePassword(){
+  //Code to generate password goes here  
+
+  //Return Randomly generated password
 }
 
 // Add event listener to generate button
